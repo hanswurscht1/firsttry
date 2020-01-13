699df2483730b9e8b01308e99affad35cdcd4efe
hanswurscht1 <bruederlie@yahoo.de>
2020-01-13 18:28:00 +0100

    version 1.0.1

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world this is version 1.0.1 of this test code\n")
 	fmt.Printf("hello bllub_rld\n")
 	fmt.Printf("hello bllub_rld\n")
 	
